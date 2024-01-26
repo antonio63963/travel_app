1eee184b2b0bd2ea1d82918fd227048274f6ce62
Anton <60780638+antonio63963@users.noreply.github.com>
2024-01-26 16:16:15 +0200

    main screen bottom app bar

--- lib/features/domain/usescases/delete_trip.dart
+++ lib/features/domain/usescases/delete_trip.dart
@@ -1,10 +1,10 @@
 import 'package:travel_app/features/domain/entities/trip.dart';
 import 'package:travel_app/features/domain/repositories/abstract_trip_repository.dart';
 
-class DeletTrip {
+class DeleteTrip {
   final AbstractTripRepository repository;
 
-  DeletTrip({required this.repository});
+  DeleteTrip({required this.repository});
 
  Future<void> call(int tripId) {
     return repository.deleteTrip(tripId);
